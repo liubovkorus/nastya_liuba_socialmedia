8951dfe6d846fab58977ad277e13b6ec2b9e0bac
liubov <lyuboffkorus@gmail.com>
2022-04-18 00:15:22 +1000

    запуск авторизации/регистрации (шаблон)

---
+++ loginform.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def log_in():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def reg_page():
+    return render_template('sign_up.html')
+
+
+if __name__ == '__main__':
+    app.run()
